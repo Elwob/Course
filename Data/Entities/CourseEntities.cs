576a1c93af5fc6e429032c79287d5a03259fe581
SabineSchropper <sabine.schropper@gmail.com>
2020-07-02 13:24:43 +0200

    Added Entities

--- Data/Entities/CourseEntities.cs
+++ Data/Entities/CourseEntities.cs
@@ -10,13 +10,19 @@ namespace Data.Entities
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Content> Contents { get; set; }
-
-
-        public DbSet<RelCourseParticipant> RelCourseParticipants{ get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<RelDocumentClass> RelDocumentClasses { get; set; }
+        public DbSet<Communication> Communications { get; set; }
+        public DbSet<RelCommunicationClass> RelCommunicationClasses { get; set; }
+        public DbSet<RelCourseParticipant> RelCourseParticipants { get; set; }
         public DbSet<RelCourseTrainer> RelCourseTrainers { get; set; }
         public DbSet<Absence> Absences { get; set; }
         public DbSet<EmailTemplate> EmailTemplates { get; set; }
-
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<RelAddressPerson> RelAddressPersons { get; set; }
+        public DbSet<Contact> Contacts{ get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -35,7 +41,7 @@ namespace Data.Entities
                 entity.Property(x => x.Title).IsRequired();
                 entity.Property(x => x.Category).IsRequired();
                 entity.Property(x => x.CreatedAt).IsRequired();
-               
+
             });
 
             modelBuilder.Entity<Document>(entity =>
@@ -60,23 +66,15 @@ namespace Data.Entities
                 entity.HasKey(x => x.Id);
                 entity.Property(x => x.CommunicationId).IsRequired();
                 entity.Property(x => x.Class).IsRequired();
-                entity.Property(x => x.ClassId).IsRequired();              
+                entity.Property(x => x.ClassId).IsRequired();
             });
-
-
-
-
-
-
-
-
 
             modelBuilder.Entity<RelCourseParticipant>(entity =>
             {
                 entity.HasKey(x => x.Id);
                 entity.Property(x => x.CourseId).IsRequired();
                 entity.Property(x => x.ParticipantId).IsRequired();
-             
+
             });
 
             modelBuilder.Entity<RelCourseTrainer>(entity =>
@@ -87,7 +85,6 @@ namespace Data.Entities
 
             });
 
-
             modelBuilder.Entity<Absence>(entity =>
             {
                 entity.HasKey(x => x.Id);
@@ -95,7 +92,6 @@ namespace Data.Entities
                 entity.Property(x => x.ParticipantId).IsRequired();
                 entity.Property(x => x.CourseId).IsRequired();
                 entity.Property(x => x.Completed).IsRequired();
-
 
             });
 
@@ -113,6 +109,51 @@ namespace Data.Entities
                 entity.Property(x => x.DocId).IsRequired();
                 entity.Property(x => x.Class).IsRequired();
                 entity.Property(x => x.ClassId).IsRequired();
+            });
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.FirstName).IsRequired();
+                entity.Property(x => x.LastName).IsRequired();
+                entity.Property(x => x.Function).IsRequired();
+                entity.Property(x => x.Active).IsRequired();
+                entity.Property(x => x.Deleted).IsRequired();
+                entity.Property(x => x.WantsNewsletter).IsRequired();
+                entity.Property(x => x.CreatedAt).IsRequired();
+            });
+
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.CreatedAt).IsRequired();
+            });
+
+            modelBuilder.Entity<RelAddressPerson>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.AdressId).IsRequired();
+                entity.Property(x => x.PersonId).IsRequired();
+            });
+
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.PersonId).IsRequired();
+                entity.Property(x => x.ArtOfCommunication).IsRequired();
+                entity.Property(x => x.ContactValue).IsRequired();
+                entity.Property(x => x.ContactType).IsRequired();
+                entity.Property(x => x.MainContact).IsRequired();
+                entity.Property(x => x.CreatedAt).IsRequired();
+            });
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.PersonId).IsRequired();
+                entity.Property(x => x.CommentValue).IsRequired();
+                entity.Property(x => x.ValueDate).IsRequired();
+                entity.Property(x => x.CreatedAt).IsRequired();
             });
 
 
