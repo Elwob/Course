3072b1df1df06d7cd4e2be7b2c7eb49457ad63d2
SabineSchropper <sabine.schropper@gmail.com>
2020-07-02 12:50:08 +0200

    Added Models

---
+++ Data/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data.Models
+{
+    [Table ("comment")]
+    public class Comment
+    {
+        [Column ("id")]
+        public int Id { get; set; }
+        [Column("person_id")]
+        public int PersonId { get; set; }
+        [Column("comment_value")]
+        public string CommentValue { get; set; }
+        [Column("value_date")]
+        public DateTime ValueDate { get; set; }
+        [Column("created@")]
+        public DateTime CreatedAt { get; set; }
+        [Column("modify@")]
+        public DateTime ModifiedAt { get; set; }
+    }
+}
---
+++ Data/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data.Models
+{
+    [Table ("contact")]
+    public class Contact
+    {
+        [Column ("id")]
+        public int Id { get; set; }
+        [Column("person_id")]
+        public int PersonId { get; set; }
+        [Column("art_of_communication", TypeName = "varchar(200)")]
+        public string ArtOfCommunication { get; set; }
+        [Column("contact_value", TypeName = "varchar(200)")]
+        public string ContactValue { get; set; }
+        [Column("contact_type", TypeName = "varchar(200)")]
+        public string ContactType { get; set; }
+        [Column("main_contact")]
+        public bool MainContact { get; set; }
+        [Column("created@")]
+        public DateTime CreatedAt { get; set; }
+        [Column("modified@")]
+        public DateTime ModifiedAt { get; set; }
+
+    }
+}
