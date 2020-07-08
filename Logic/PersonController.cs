f77e45c18f4aa65fc854dd905a44d384815d521e
Your Name <you@example.com>
2020-07-08 14:12:05 +0200

    working Email Copy pdf

---
+++ Logic/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    class PersonController: MainController
+    {
+   
+        public Person FindOne(int id)
+        {
+            
+            return entities.Persons.FirstOrDefault(x => x.Id == id);
+        }
+
+    }
+}
