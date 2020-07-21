03c831c45940c6766e8b9aeae79485f86d0bbb20
SabineSchropper <sabine.schropper@gmail.com>
2020-07-21 12:51:53 +0200

    Added MissingInputException and Exception Handling in CommunicationController

---
+++ Logic/Exceptions/MissingInputException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Exceptions
+{
+    public class MissingInputException : Exception
+    {
+        public MissingInputException() : base() { }
+        public MissingInputException(string message) : base(message) { }
+        public MissingInputException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
