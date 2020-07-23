60535dd76e5941cde60efb801fee09756de22e0d
Your Name <you@example.com>
2020-07-23 13:58:15 +0200

    change Email replace

--- Logic/EmailTemplates.cs
+++ Logic/EmailTemplates.cs
@@ -12,37 +12,25 @@ namespace Logic
             //     int document template number
             int getDocumentNr = (int)emailTemplate.DocumentType;
             Course course = entities.Courses.FirstOrDefault(id => id.Id == emailTemplate.CourseId);
-
-            // fill database template for diploma Email
-            if ((int)emailTemplate.DocumentType == 5)
-            {
                 String courseName = course.Title;
                 EmailTemplate emailTemplateForText = entities.EmailTemplates.FirstOrDefault(id => id.Id == getDocumentNr);
-                string body = emailTemplateForText.Text;
+            DateTime datenow = DateTime.Now;
+            DateTime courseTime = course.Start.Value;
+         
+            string body = emailTemplateForText.Text;
+
                 body = body.Replace("{Geschlecht}", person.Gender);
                 body = body.Replace("{Vorname}", person.FirstName);
                 body = body.Replace("{Nachname}", person.LastName);
                 body = body.Replace("{Kurstitel}", courseName);
-                message.Body = body;
-            }
-
-            //
-            if ((int)emailTemplate.DocumentType == 6)
-            {
-                EmailTemplate emailTemplateForText = entities.EmailTemplates.FirstOrDefault(id => id.Id == getDocumentNr);
-                string body = emailTemplateForText.Text;
-                String courseName = course.Title;
-                DateTime datenow = DateTime.Now;
-                DateTime courseTime = course.Start.Value;
                 body = body.Replace("{Datenow}", datenow.ToString());
-                body = body.Replace("{Vorname}", person.FirstName);
-                body = body.Replace("{Nachname}", person.LastName);
                 body = body.Replace("{Eventname}", courseName);
                 body = body.Replace("{Day}", courseTime.DayOfYear.ToString());
                 body = body.Replace("{Time}", courseTime.Hour.ToString());
-                //         body = body.Replace("{Street}", course.CourseClassrooms.ToString());
-                //        body = body.Replace("{Street}", course.CourseClassrooms.ToString());
-            }
+                body = body.Replace("{Month}", courseTime.Month.ToString());
+                body = body.Replace("{Street}", course.CourseClassrooms.ToString());
+            message.Body = body;
+        
             return message;
         }
     }
